190974b531d64467c0ff7f1d755c4fc121aedf25
robinkjong <robinkjong@hotmail.com>
2016-09-10 15:15:14 +1000

    Construct initial input interface

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log('Hello World'); //Test Line
+
+//Import readline module
+var readline = require("readline");
+
+//Construct instances of readline.Interface class
+var prompts = readline.createInterface(process.stdin, process.stdout);
+
+//Input Interface
+prompts.question("Please input your commands here?", function (userInput) {
+
+    if (userInput.substring(0,5) == "PLACE"){
+        message = "Valid command, continue your input";
+    } else {
+        message = "Invalid command";
+    }
+    console.log(message);
+
+    //Check if user input "REPORT"
+    var isREPORT = (userInput == 5);
+    if (isREPORT) {
+        process.exit();
+    }
+});
