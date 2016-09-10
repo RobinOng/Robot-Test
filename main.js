1683273518b33eda62c1eb079e1d3e96b52b4288
robinkjong <robinkjong@hotmail.com>
2016-09-10 17:23:56 +1000

    Updated first input validation
    Added validation for comma(s)

--- main.js
+++ main.js
@@ -12,14 +12,22 @@ askFirstInput();
 function askFirstInput(){
     prompts.question("Please input your first commands here:", function (userInput) {
 
-        var isPLACE = (userInput.substring(0,5)=="PLACE");
-        var isSpace = (userInput.substring(5,6)=="");
-        var isX = (userInput.substring(6,7)>=0 && userInput.substring(6,7)<=5);
-        var isY = (userInput.substring(8,9)>=0 && userInput.substring(8,9)<=5);
-        var isF = (userInput.substring(10,15)=="NORTH" || userInput.substring(10,15)=="SOUTH" || userInput.substring(10,15)=="EAST" || userInput.substring(10,15)=="WEST");
+        //Extract userInput
+        var X = userInput.substring(6,7);
+        var Y = userInput.substring(8,9);
+        var F = userInput.substring(10,15);
 
-        if (isPLACE && isSpace && isX && isY && isF){
-            validateFirstInput();
+        //Validate userInput
+        var isPLACE = (userInput.substring(0,5)=="PLACE");
+        var isSPACE = (userInput.substring(5,6)==" ");
+        var isCOMMA = (userInput.substring(7,8)=="," && userInput.substring(9,10)==",");
+        var isX = (X>=0 && X<=5);
+        var isY = (Y>=0 && Y<=5);
+        var isF = (F=="NORTH" || F=="SOUTH" || F=="EAST" || F=="WEST");
+
+        if (isPLACE && isSPACE && isCOMMA && isX && isY && isF){
+            message = "Valid command, continue your input:";
+            console.log(message);
             process.exit();
         } else {
             message = "Invalid command, please input a new command:";
@@ -27,9 +35,4 @@ function askFirstInput(){
             askFirstInput();
         }
     })
-}
-
-function validateFirstInput(){
-    message = "Valid command, continue your input:";
-    console.log(message);
 }
